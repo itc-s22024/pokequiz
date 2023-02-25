465bfff9919a2e23ce3712af70cde9afd7b5c5fc
s22024 <s22024@std.it-college.ac.jp>
2023-02-25 09:54:42 +0000

    画像と名前の取得完了

---
+++ components/fetchPokemonData.js
@@ -0,0 +1,12 @@
+import fetch from 'isomorphic-fetch';
+
+export default async function fetchPokemonData(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await response.json()
+    const imag = await data.sprites.other['official-artwork'].front_default
+
+    const nameresponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+    const namedata = await nameresponse.json()
+    const name = await namedata.names.find((name) => name.language.name === 'ja').name
+    return {imag, name}
+}
